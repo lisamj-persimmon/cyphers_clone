dfdd952ac6a88210fb7aaab86abf5400d97a724a
lisamj-persimmon <66669089+lisamj-persimmon@users.noreply.github.com>
2025-09-01 19:03:52 +0900

    프로젝트 환경설정
    mysql, logj4 등

---
+++ src/test/java/com/test/persistence/JDBCTest.java
@@ -0,0 +1,38 @@
+package com.test.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class JDBCTest {
+	
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try(
+			Connection con = 
+			DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1:3306/test_db?serverTimezone=Asia/Seoul",
+					"lisamj",
+					"1247mj18!!")
+				
+				
+		){
+			System.out.println(con);
+		}catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+}
